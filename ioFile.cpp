62627b07b9564413ece941b28c4e635e8a5e3a9a
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-06-09 14:39:45 +0700

    membuat class baris ofstream dan membuka filenya dengan mode penulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,5 +4,14 @@
 using namespace std;
 
 int main() {
+        string baris;
+
+        //membuka file dalam mode menulis.
+        ofstream outfile;
+        //menunjukan ke dalam sebuah nama file
+        outfile.open("contohfile.txt");
+
+        cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
 }
